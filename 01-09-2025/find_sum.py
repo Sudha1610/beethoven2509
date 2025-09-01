c50b4979d143abc63ec410eb039cdc2d253a0f1c
Sudha Ramadurgam (suramadu) <suramadu@cisco.com>
2025-09-01 10:59:04 +0530

    hello

---
+++ 01-09-2025/find_sum.py
@@ -0,0 +1,10 @@
+#find_sum 
+'''
+find_sum
+args:firstnumber,secondnumber
+returns sum of all numbers
+'''
+def find_sum(firstnumber,secondnumber):
+    result = firstnumber + secondnumber
+    return result 
+print(find_sum(100,10))
