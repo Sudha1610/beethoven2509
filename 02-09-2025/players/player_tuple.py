664eb2dd49481dce9d63df3a91ba32d805ab2051
Sudha1610 <rsudha1610@gmail.com>
2025-09-02 16:40:22 +0530

    hello

---
+++ 02-09-2025/players/player_tuple.py
@@ -0,0 +1,24 @@
+players = [
+    (101, 'jaiswal'),
+    (102, 'gill')
+]
+print(players)
+#player=(players)
+
+player = {}
+player = (103, 'abhishek')
+players.append(player)
+
+
+
+
+#players.append(player)
+#print(players)
+
+for player in players:
+    if player[0] == 103:
+        print(player)
+
+players_dict = { 101:players[0],102:players[1],103:players[2]}
+
+print(players_dict[103])
